5b588cac289d064e9831177822e4224da8d9a046
Tamojit Das <tamojitdas181007@gmail.com>
2025-02-15 00:05:16 +0530

    App init

---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express, { Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("common"));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+app.get("/api/v1", (req: Request, res: Response) => {
+    res.send("<h1>Server up & Running</h1>");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
